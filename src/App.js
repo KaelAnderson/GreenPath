006cc9efc838fef639018d038ba283fc8a9fead0
kizukizu <taramcb24@gmail.com>
2024-02-22 16:21:05 -0500

    blank react page opens on localhost

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import 'react';
+import 'axios';
+import './App.css';
+import './index.html';
+
+
+function App() {
+
+
+return (
+    <div>
+        <div>
+            <h1>hello greepath nation</h1>
+        </div>
+    </div>
+)
+}
+
+export default App;
