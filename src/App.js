3ff793c228fb7d89e04bf94da76be19867723e24
kizukizu <taramcb24@gmail.com>
2024-02-23 00:15:58 -0500

    window.google

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import React from 'react';
-import axios from 'axios'; // assuming we'll use it (?)
+import { useState, useEffect } from 'react';
+//import axios from 'axios'; // assuming we'll use it (?)
 import './App.css';
 import './index.css';
-
+const google = window.google;
 
 
 function App() {
-  let map;
+  const [genMap, setGenMap] = useState(1);
 
-  async function initMap() {
-    // The location of Uluru
-    const position = { lat: -25.344, lng: 131.031 };
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-    // The map, centered at Uluru
-    map = new Map(document.getElementById("map"), {
-      zoom: 4,
-      center: position,
-      mapId: "DEMO_MAP_ID",
-    });
-
-    // The marker, positioned at Uluru
-    const marker = new AdvancedMarkerElement({
-      map: map,
-      position: position,
-      title: "Uluru",
-    });
+  const handleGenMap = (event) => {
+    setGenMap((prev) => prev * -1);
   }
 
-  initMap();
+  useEffect(() => {
+    let map;
+
+    async function initMap() {
+      // The location of Uluru
+      const position = { lat: -25.344, lng: 131.031 };
+      // Request needed libraries.
+      //@ts-ignore
+      const { Map } = await google.maps.importLibrary("maps");
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+      // The map, centered at Uluru
+      map = new Map(document.getElementById("map"), {
+        zoom: 4,
+        center: position,
+        mapId: "DEMO_MAP_ID",
+      });
+
+      // The marker, positioned at Uluru
+      const marker = new AdvancedMarkerElement({
+        map: map,
+        position: position,
+        title: "Uluru",
+      });
+    }
+
+    initMap();
+  }, [genMap]);
   //perhaps regen map when destination state is updated using a useeffect hook
 
 
@@ -39,6 +48,7 @@ function App() {
     <div className="App">
       
       <h1>hello greepath nation</h1>
+      <button onClick={handleGenMap}>Generate Map</button>
       <div id="mapDiv">
 
       </div>
@@ -46,6 +56,5 @@ function App() {
   
     );
   }
-
 
 export default App;
